7c670db7c44ce4363e2b3a4ba5ac419353cd474a
sierratango34 <k.sritawan@gmail.com>
2019-05-12 12:10:38 +0100

    airline enum class

---
+++ src/main/java/Airline.java
@@ -0,0 +1,13 @@
+public enum Airline {
+
+    BA,
+    KLM,
+    RYANAIR,
+    EASYJET,
+    DELTA,
+    EMIRATES,
+    ETIHAD,
+    FLYBE,
+    JETSTAR
+
+}
--- src/main/java/Plane.java
+++ src/main/java/Plane.java
@@ -1,2 +1,5 @@
 public class Plane {
+
+    private Airline airline;
+
 }
