792562ebc7664372c95805ec87765c9496881fcc
sierratango34 <k.sritawan@gmail.com>
2019-05-12 13:23:46 +0100

    Airport: getAirportCode test pass

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Airport {
+
+    private String code;
+    private ArrayList<Hangar> hangars;
+    private ArrayList<Flight> flights;
+
+    public Airport(String code) {
+        this.code = code;
+        this.hangars = new ArrayList<>();
+        this.flights = new ArrayList<>();
+    }
+
+    public String getAirportCode() {
+        return this.code;
+    }
 }
--- src/test/java/AirportTest.java
+++ src/test/java/AirportTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class AirportTest {
+
+    Airport airport1;
+
+    @Before
+    public void before(){
+        airport1 = new Airport("GLA");
+    }
+
+    @Test
+    public void hasAirportCode(){
+        assertEquals("GLA", airport1.getAirportCode());
+    }
 }
