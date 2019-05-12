eb545d78e06f6fdd10375175dd591c3ef7f35302
sierratango34 <k.sritawan@gmail.com>
2019-05-12 13:17:02 +0100

    Flight: hasFlightNumber test pass

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,16 @@
 public class Flight {
+
+    private Plane plane;
+    private int flightNumber;
+    private String destination;
+
+    public Flight(int flightNumber, String destination) {
+        this.plane = null;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+    }
+
+    public int getFlightNumber() {
+        return this.flightNumber;
+    }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+
+    Flight flight1;
+
+    @Before
+    public void before(){
+        flight1 = new Flight(123, "Paris");
+    }
+
+    @Test
+    public void hasFlightNumber(){
+        assertEquals(123, flight1.getFlightNumber());
+    }
 }
