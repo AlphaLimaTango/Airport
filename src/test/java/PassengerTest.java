a2b31eab8a0cd26f54a276999a898573d95275e2
sierratango34 <k.sritawan@gmail.com>
2019-05-12 14:44:34 +0100

    Passenger: canBuyTicket test pass

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -6,10 +6,12 @@ import static org.junit.Assert.assertEquals;
 public class PassengerTest {
 
     Passenger passenger1;
+    Flight flight1;
 
     @Before
     public void before(){
         passenger1 = new Passenger("Jen");
+        flight1 = new Flight(8, "Dubai");
     }
 
     @Test
@@ -20,5 +22,11 @@ public class PassengerTest {
     @Test
     public void startsWithNoTickets(){
         assertEquals(0, passenger1.getTicketCount());
+    }
+
+    @Test
+    public void canBuyTicket(){
+        passenger1.buyTicket(flight1);
+        assertEquals(1, passenger1.getTicketCount());
     }
 }
